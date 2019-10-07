b3c7a7e3923fb7ec88019f0cbc3122ca0fe8496e
Bob Frost <bob@DESKTOP-BB7PTIN.localdomain>
2019-10-07 13:15:56 -0400

    initial team load

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,11 @@
 class TeamsController < ApplicationController
+
+  def import
+    ActiveRecord::Base.transaction do
+      return render json: { message: "No input file specified"}, status: :unprocessable_entity if params[:file] == nil
+      @teams, @members = Team.import(params[:file])
+    end
+    render json: { teams: @teams, members: @members }, status: :created
+   end
+
 end
