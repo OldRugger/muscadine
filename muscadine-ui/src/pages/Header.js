501d4852b128606cdba2afbd244ec328ad0d68f8
Bob Frost <bob.a.frost@gmail.com>
2019-10-30 19:35:40 -0400

    initial api call (config data)

---
+++ muscadine-ui/src/pages/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h3>2020 Southeastern Interscholastic Championships</h3>
+      </header>
+    );
+  }
+}
--- muscadine-ui/src/pages/HomePage.js
+++ muscadine-ui/src/pages/HomePage.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Header from "./Header";
+
 import "../App.css";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h3>2020 Southeastern Interscholastic Championships</h3>
-        </header>
+        <Header />
         <p>
           <Link to="/config">Configuration</Link>
         </p>
